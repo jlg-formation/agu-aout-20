53e99087eaa41fcbcbfbc2956a7b048911204b13
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-08-25 16:03:02 +0200

    added server script

--- back/server.js
+++ back/server.js
@@ -1,1 +1,14 @@
-console.log('starting server');
+console.log("starting server");
+
+const express = require("express");
+const serveIndex = require("serve-index");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("."));
+app.use(serveIndex(".", { icons: true }));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
