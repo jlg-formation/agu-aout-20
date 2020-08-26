afa84399bbc15ca6fb3c07019cf104b535e56918
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-08-26 13:50:24 +0200

    added header component

--- front/src/app/layout/layout.module.ts
+++ front/src/app/layout/layout.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [
     CommonModule
   ]
