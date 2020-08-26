1b0142e1a57dc89cc9b7d2ca260075980d5e2fe9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-08-26 09:07:48 +0200

    added angular

--- back/server.ts
+++ back/server.ts
@@ -1,5 +1,5 @@
-const express = require("express");
-const serveIndex = require("serve-index");
+import express from "express";
+import serveIndex from "serve-index";
 
 const app = express();
 const port = 3000;
