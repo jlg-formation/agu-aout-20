7d7bdb2b05cc636b8a07e171394513afc01fc932
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-08-26 17:10:48 +0200

    routing 2: adding routermodule

--- front/src/app/layout/layout.module.ts
+++ front/src/app/layout/layout.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, BodyComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [HeaderComponent, FooterComponent, BodyComponent],
 })
 export class LayoutModule {}
