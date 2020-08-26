041243abf2cc80dfc300fdcb45dedb86710d7263
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-08-26 13:30:25 +0200

    prod ok

--- back/server.ts
+++ back/server.ts
@@ -3,9 +3,10 @@ import serveIndex from "serve-index";
 
 const app = express();
 const port = 3000;
+const www = "../front/dist/front";
 
-app.use(express.static("."));
-app.use(serveIndex(".", { icons: true }));
+app.use(express.static(www));
+app.use(serveIndex(www, { icons: true }));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
