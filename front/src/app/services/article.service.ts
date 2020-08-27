d75ad09fa776c5371cab9398261169e10b6c0ed9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-08-27 15:54:03 +0200

    article refresh ok

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -5,15 +5,28 @@ import { Article } from '../interfaces/article';
   providedIn: 'root',
 })
 export class ArticleService {
-  articles: Article[] = [
-    { name: 'Tournevis', price: 2.44, qty: 140 },
-    { name: 'Tournevis cruciforme', price: 2.78, qty: 110 },
-    { name: 'Marteau', price: 1.2, qty: 25 },
-    { name: 'Pince', price: 4.21, qty: 13 },
-  ];
+  articles: Article[] = this.getArticles();
   constructor() {}
+
+  save(): void {
+    localStorage.setItem('articles', JSON.stringify(this.articles));
+  }
+
+  getArticles(): Article[] {
+    const str = localStorage.getItem('articles');
+    if (!str) {
+      return [
+        { name: 'Tournevis', price: 2.44, qty: 140 },
+        { name: 'Tournevis cruciforme', price: 2.78, qty: 110 },
+        { name: 'Marteau', price: 1.2, qty: 25 },
+        { name: 'Pince', price: 4.21, qty: 13 },
+      ];
+    }
+    return JSON.parse(str) as Article[];
+  }
 
   add(a: Article): void {
     this.articles.push(a);
+    this.save();
   }
 }
