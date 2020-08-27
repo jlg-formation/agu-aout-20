8d9b92bca59df1be97dea6666f8cf8bc7af75389
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-08-27 10:14:27 +0200

    preload strategy

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+
 import { HomeComponent } from './routes/home/home.component';
 import { LegalComponent } from './routes/legal/legal.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'legal', component: LegalComponent },
-  { path: 'stock', loadChildren: () => import('./stock/stock.module').then(m => m.StockModule) },
+  {
+    path: 'stock',
+    loadChildren: () =>
+      import('./stock/stock.module').then((m) => m.StockModule),
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules,
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
