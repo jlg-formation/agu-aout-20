584d765c7c057a556bf9ac793b9b827fb8930289
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-08-27 14:21:31 +0200

    article from service

---
+++ front/src/app/services/article.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Article } from '../interfaces/article';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ArticleService {
+  articles: Article[] = [
+    { name: 'Tournevis', price: 2.44, qty: 140 },
+    { name: 'Tournevis cruciforme', price: 2.78, qty: 110 },
+    { name: 'Marteau', price: 1.2, qty: 25 },
+    { name: 'Pince', price: 4.21, qty: 13 },
+  ];
+  constructor() {}
+}
--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faRedoAlt, faPlus } from '@fortawesome/free-solid-svg-icons';
 
-import { Article } from 'src/app/interfaces/article';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-stock',
@@ -12,14 +12,7 @@ export class StockComponent implements OnInit {
   faRedoAlt = faRedoAlt;
   faPlus = faPlus;
 
-  articles: Article[] = [
-    { name: 'Tournevis', price: 2.44, qty: 140 },
-    { name: 'Tournevis cruciforme', price: 2.78, qty: 110 },
-    { name: 'Marteau', price: 1.2, qty: 25 },
-    { name: 'Pince', price: 4.21, qty: 13 },
-  ];
-
-  constructor() {}
+  constructor(public articleService: ArticleService) {}
 
   ngOnInit(): void {}
 }
