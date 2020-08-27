f1208c2932ac026bcdc2c26715e14b5ebbb11d09
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-08-27 13:51:23 +0200

    added article interface

---
+++ front/src/app/interfaces/article.ts
@@ -0,0 +1,5 @@
+export interface Article {
+  name: string;
+  price: number;
+  qty: number;
+}
--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { faRedoAlt, faPlus } from '@fortawesome/free-solid-svg-icons';
 
+import { Article } from 'src/app/interfaces/article';
+
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
   faRedoAlt = faRedoAlt;
   faPlus = faPlus;
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  articles: Article[] = [
+    { name: 'Tournevis', price: 2.44, qty: 140 },
+    { name: 'Tournevis cruciforme', price: 2.78, qty: 110 },
+    { name: 'Marteau', price: 1.2, qty: 25 },
+    { name: 'Pince', price: 4.21, qty: 13 },
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
