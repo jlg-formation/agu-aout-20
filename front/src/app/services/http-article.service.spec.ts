cc68c522da78941656562ae7e191797da5ab3fad
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-08-28 14:28:20 +0200

    http testing

--- front/src/app/services/http-article.service.spec.ts
+++ front/src/app/services/http-article.service.spec.ts
@@ -1,19 +1,38 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
-import { HttpArticleService } from './http-article.service';
+import { HttpArticleService, url } from './http-article.service';
 
 describe('HttpArticleService', () => {
   let service: HttpArticleService;
+  let http: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(HttpArticleService);
+    http = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    http.verify();
   });
 
   it('should be created', () => {
+    const req = http.expectOne(url);
+    expect(req.request.method).toEqual('GET');
+    req.flush([]);
+    expect(service).toBeTruthy();
+  });
+
+  it('should go in error 404', () => {
+    const req = http.expectOne(url);
+    expect(req.request.method).toEqual('GET');
+    req.flush('', { status: 404, statusText: 'not found' });
     expect(service).toBeTruthy();
   });
 });
--- front/src/app/services/http-article.service.ts
+++ front/src/app/services/http-article.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ArticleService } from './article.service';
 import { Article } from '../interfaces/article';
 
-const url = 'http://localhost:3000/ws/articles';
+export const url = 'http://localhost:3000/ws/articles';
 
 @Injectable({
   providedIn: 'root',
